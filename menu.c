dc8d229097152067a2583bd4a2264979e2dc3515
beyzakececi <beyzakececi.35@gmail.com>
2022-10-18 23:51:15 +0300

    first commit

---
+++ menu.c
@@ -0,0 +1,160 @@
+#include <stdio.h>
+#include <stdlib.h>
+//Beyza Kececi 210229001
+
+// A linked list node
+struct Node{
+    int data;
+    struct Node *next;
+};
+
+//inserts a new node on the front of the list
+void push(struct Node** headRef,int newData){
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = newData;
+    newNode->next = (*headRef);
+    (*headRef) = newNode;
+}
+//insert a new node after the given node
+void insertAfter(struct Node* prevNode, int newData)
+{
+    //check if the given prevNode is NULL 
+	if (prevNode == NULL)
+	{
+		printf("the given previous node is NULL!!!");
+		return;
+	}
+
+	struct Node* newNode =(struct Node*) malloc(sizeof(struct Node));
+
+	newNode->data = newData;
+
+	newNode->next = prevNode->next;
+
+	prevNode->next = newNode;
+}
+//insert a new node at the end
+void append(struct Node** headRef, int newData)
+{
+	struct Node* newNode = (struct Node*) malloc(sizeof(struct Node));
+
+	struct Node *last = *headRef;
+
+	newNode->data = newData;
+
+	newNode->next = NULL;
+
+	if (*headRef == NULL)
+	{
+		*headRef = newNode;
+		return;
+	}
+    
+    //travel till the last node 
+	while (last->next != NULL){
+		last = last->next;
+	}
+		
+	last->next = newNode;
+	return;
+}
+
+//Print reverse of a Linked List
+ void revPrintList(struct Node *headRef){
+ 	
+    if (headRef == NULL)
+       return;
+ 
+    revPrintList(headRef->next);
+    printf(" %d ", headRef->data);
+}
+  
+//Print a linked list
+void printLinkedList(struct Node *node){
+    while(node != NULL)
+    {
+        printf(" %d ",node->data);
+        node= node->next;
+    }
+}
+ //Counts no. of nodes in linked list
+int lengthList(struct Node* headRef){
+	
+	int counter=0;
+	
+    struct Node* tmp = headRef;
+    while (tmp != NULL) {
+        counter++;
+        tmp = tmp->next;
+    }
+    return printf("\ncount of nodes: %d\n",counter);
+}
+
+//Delete all elements with the same value
+void deleteAllNodes(struct Node *head)
+{
+	//Define some auxiliary variables
+	struct Node *newHead = NULL;
+	struct Node *outer = head;
+	struct Node *inner = NULL;
+	//Resultant variables
+	int status = 0;
+	int counter = 1;
+	printf(" After delete duplicate elements : ");
+	//Below process takes O(n*n) time
+	//iterating linked list elements
+	while (outer != NULL)
+	{
+		inner = head;
+		//iterating linked list elements
+		while (inner != NULL && counter <= 1)
+		{
+			if (inner != outer && inner->data == outer->data)
+			{
+				//When element are duplicate
+				counter++;
+			}
+			//visit to next node
+			inner = inner->next;
+		}
+		if (counter == 1)
+		{
+			status = 1;
+			append(&newHead,outer->data);
+		}
+		//reset the counter value
+		counter = 1;
+		//visit to next node
+		outer = outer->next;
+	}
+
+	*head = *newHead;
+}
+
+int main(int argc, char *argv[]) {
+	
+	struct Node* head = NULL;
+	push(&head,23);
+    push(&head,27);
+    push(&head,27);
+    push(&head,30);
+    push(&head,30);
+    push(&head,220);
+    push(&head,220);
+    push(&head,30);
+    insertAfter(head->next,37);
+    append(&head,84);
+    printf("\n Created Linked list is: \n");
+    printLinkedList(head);
+    printf("\n Reversed Linked list is: \n");
+    revPrintList(head);
+    lengthList(head);
+    printf("Before delete duplicate nodes: \n");
+    printLinkedList(head);
+    printf("\n");
+    deleteAllNodes(head);
+    printf("\n");
+    printLinkedList(head);
+	
+	return 0;
+}
